0820f31d35a6c22762833d8fe465a2cf8b8da650
ScorchedTrace <53928510+SchorchedTrace@users.noreply.github.com>
2020-11-04 23:42:36 +0700

    go back dorm first

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,27 @@ function end() {
 //     setTimeout(document.body.classList.remove("shake"), 3000);
 //     setTimeout(document.body.classList.add("shake"), 3000);
 // }
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            if (entry.intersectionRatio > 0) {
+                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+            } else {
+                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+            }
+        });
+    });
+
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('section[id]').forEach((section) => {
+        observer.observe(section);
+    });
+    
+});
+
+
+
+Resources1×0.5×0.25×Rerun
